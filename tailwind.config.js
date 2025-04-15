b06cee8c562b9f84d675cb86a94afc8010003698
Suhana <suhanakesharwani@gmail.com>
2025-04-16 03:23:17 +0530

    🎉 Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
